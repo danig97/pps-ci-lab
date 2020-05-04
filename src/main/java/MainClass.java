e17680f461d3b6a96fb452b66c38471262e50563
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-05-04 15:35:58 +0200

    Add some comments and a new function.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,18 @@
 public class MainClass {
+	/**
+	 * Log a message on the output.
+	 */
+	public static void log(String msg) {
+		synchronized(System.out) {
+			System.out.println(msg);
+		}
+	}
+
+	/**
+	 * Execute the program.
+	 */
 	public static void main(String[] args) {
 		// Stampa solo Hello World.
-		System.out.println("Hello World!");
+		log("Hello World!");
 	}
 }
